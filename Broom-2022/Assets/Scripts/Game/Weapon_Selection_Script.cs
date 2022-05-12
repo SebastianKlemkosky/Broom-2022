ed02af300bc09bc9de307ccc23ec36f83c1d4950
Sebs <61159145+SebastianKlemkosky@users.noreply.github.com>
2022-05-12 06:42:44 -0500

    5/12/2022 part 2
    Added Orange
    Added rambles
    Added empty script

---
+++ Broom-2022/Assets/Scripts/Game/Weapon_Selection_Script.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This Script Handles Weapon Selection
+public class Weapon_Selection_Script : MonoBehaviour
+{
+
+    //This script needs hold a reference to all the weapon models
+    //Get a list of all the child objects in the weapons GameObject 
+
+
+    //activates the current weapon, the deactivates the others
+
+    //Using said list do this ^
+
+    //Also needs to switch the weapons projectile/ bullets / method of damage to the current weapon's one and deactives the others
+    //Will need to make use of the object pooling manager, and projectile bullet scripts
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
