b71e1ffdd6cb5b505e8c07482cde93843599443e
Sebs <61159145+SebastianKlemkosky@users.noreply.github.com>
2022-05-30 16:46:06 -0500

    5/30
    Create a raycast weapon script to add variety to the weapons
    Can be used to create various types of raycast weapons
    Need to add damage to weapon from the enemy script
    5:13 in video
    Need to edit the projectile weapons to be confined to the guns instead of the whole player
    Also need to add particle effects to possibly screen shake to some guns

---
+++ Broom-2022/Assets/Scripts/Game/Raycast_Gun_System_Script.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast_Gun_System_Script : MonoBehaviour
+{
+    //Gun Stats
+    public int damage;
+    public float time_Between_Shooting, spread, range, reload_Time, time_Between_Shots;
+    public int magazine_Size, bullets_Per_Tap;
+    public bool allow_Button_Hold;
+    int bullets_Left, bullets_Shot;
+
+    //bools 
+    bool shooting, ready_To_Shoot, reloading;
+
+
+    //References
+    //To to link these in code
+    public Camera player_Camera;
+    public Transform attack_Point;
+    public RaycastHit ray_Hit;
+    public LayerMask what_Is_Enemy;
+
+
+    //Graphics 
+    //Can Add Camera Shake later if needed at 5:20
+    //Can Add Other Graphic stuff later
+
+    private void Start()
+    {
+        bullets_Left = magazine_Size;
+        ready_To_Shoot = true; 
+    }
+
+    private void Update()
+    {
+        MyInput();
+        //Set the Ammo count and mag stuff later
+    }
+
+    private void MyInput()
+    {
+        if (allow_Button_Hold)
+        {
+            shooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else
+        {
+            shooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.R) && bullets_Left < magazine_Size && !reloading)
+        {
+            Reload();
+        }
+
+
+        //Shoot
+        if (ready_To_Shoot && shooting && !reloading && bullets_Left > 0)
+        {
+            bullets_Shot = bullets_Per_Tap;
+            Shoot();
+        }
+
+    }
+
+
+    private void Shoot()
+    {
+        ready_To_Shoot = false;
+
+        //Spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //Calculate Direction with Spread
+        Vector3 direction = player_Camera.transform.forward + new Vector3(x, y, 0);
+
+
+        //Raycast
+        if(Physics.Raycast(player_Camera.transform.position, direction, out ray_Hit, range, what_Is_Enemy))
+        {
+            Debug.Log(ray_Hit.collider.name);
+
+
+            //Here Apply Damage to enemy if hit
+            
+            
+
+        }
+
+        //Input Graphics Here
+
+
+
+        bullets_Left--;
+        bullets_Shot--;
+
+        Invoke("ResetShot", time_Between_Shooting);
+
+
+        if (bullets_Shot > 0 && bullets_Left > 0)
+        {
+            Invoke("Shoot", time_Between_Shots);
+        }
+
+    }
+
+
+    private void ResetShot()
+    {
+        ready_To_Shoot = true;
+    }
+
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reload_Time);
+    }
+
+    private void ReloadFinished()
+    {
+        bullets_Left = magazine_Size;
+        reloading = false;
+    }
+
+
+
+
+
+
+}
